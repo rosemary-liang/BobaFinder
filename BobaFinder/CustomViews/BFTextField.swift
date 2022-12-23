c39483949080131027dad7e4b3be0c5d7936cda8
Rosemary Liang <rosemaryliang18@gmail.com>
2022-12-23 11:39:28 -0800

    SearchVC configure function. Added logo-image

--- BobaFinder/CustomViews/BFTextField.swift
+++ BobaFinder/CustomViews/BFTextField.swift
@@ -11,6 +11,7 @@ class BFTextField: UITextField {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
--- BobaFinder/SearchVC.swift
+++ BobaFinder/SearchVC.swift
@@ -9,19 +9,23 @@ import UIKit
 
 class SearchVC: UIViewController {
     
-    var logoImageView   = UIImageView()
-    var textField       = BFTextField()
-    var actionButton    = BFButton(backgroundColor: .systemIndigo, title: "Find Boba")
+    var logoImageView       = UIImageView()
+    var zipcodeTextField    = BFTextField()
+    var actionButton        = BFButton(backgroundColor: .systemIndigo, title: "Find Boba")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         configure()
     }
     
     func configure() {
+        
+        let padding: CGFloat = 50
+        
         view.addSubview(logoImageView)
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        logoImageView.image = UIImage(named: "no-image-available")
+        logoImageView.image = UIImage(named: "logo-image")
         
         NSLayoutConstraint.activate([
             logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
@@ -31,11 +35,29 @@ class SearchVC: UIViewController {
         ])
         
         
+        view.addSubview(zipcodeTextField)
+        
+        NSLayoutConstraint.activate([
+            zipcodeTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: padding),
+            zipcodeTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            zipcodeTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            zipcodeTextField.heightAnchor.constraint(equalToConstant: padding) // for a large touch target
+        ])
+        
+        
         view.addSubview(actionButton)
+        actionButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            actionButton.topAnchor.constraint(equalTo: zipcodeTextField.bottomAnchor, constant: padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: padding)
+        ])
         
         
         
-        view.addSubview(textField)
+        
     }
     
 
