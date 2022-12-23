4b13e785bdb87e63b0b94a4c1d954042f82f5998
Rosemary Liang <rosemaryliang18@gmail.com>
2022-12-23 09:46:30 -0800

    set up NavigationController for SearchVC

---
+++ BobaFinder/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  BobaFinder
+//
+//  Created by Eric Liang on 12/23/22.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+}
