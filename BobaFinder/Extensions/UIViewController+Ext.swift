37d1610b22a5e6dcf4be5d0c38bb85601920ab88
Rosemary Liang <rosemaryliang18@gmail.com>
2022-12-23 16:36:06 -0800

    BFAlertVC configure containerView, title label, message, and action button

---
+++ BobaFinder/Extensions/UIViewController+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController+Ext.swift
+//  BobaFinder
+//
+//  Created by Eric Liang on 12/23/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentBFAlertOnMainThread() {
+        
+    }
+}
