661181927c9670e97a9fe09c6548c4a4e1741878
Rosemary Liang <rosemaryliang18@gmail.com>
2022-12-30 10:16:14 -0800

    set up getFavorites and viewWillAppear on FavoritesListVC

--- BobaFinder/Screens/FavoritesListVC.swift
+++ BobaFinder/Screens/FavoritesListVC.swift
@@ -8,11 +8,54 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
+    
+    let tableView = UITableView()
+    var favorites: [Place] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
 
-        view.backgroundColor = .systemBlue
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        getFavorites()
+    }
+    
+    
+    private func configureViewController() {
+        view.backgroundColor    = .systemBackground
+        title                   = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+    func getFavorites() {
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let favorites):
+                if favorites.isEmpty {
+                    self.showEmptyStateView(with: "No favorites yet.", in: self.view, scaleX: 1, scaleY: 1)
+                    
+                } else {
+                    self.favorites = favorites
+                    
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                        self.view.bringSubviewToFront(self.tableView)
+                    }
+                }
+                
+                
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.presentBFAlert(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+                }
+            }
+        }
     }
     
 
